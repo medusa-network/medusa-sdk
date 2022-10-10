d1d22ec86bced2368a2af9797d4ea10290e4b5f7
Jonathan Easterman <jonathaneasterman@gmail.com>
2022-10-10 18:36:28 +0100

    export defaultCurve and calculateKeyPair

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import { BigNumber, utils } from "ethers";
 import { Point, Scalar, Curve } from "./algebra";
+import { curve as defaultCurve } from "./bn254";
 
 export type PublicKey<S extends Scalar> = Point<S>;
 export type SecretKey = Scalar;
@@ -7,6 +9,8 @@ export interface KeyPair<S extends Scalar, P extends Point<Scalar>> {
   secret: S;
   pubkey: P;
 }
+
+export { defaultCurve };
 
 export function newKeypair<
   S extends Scalar,
@@ -20,6 +24,37 @@ export function newKeypair<
     pubkey: pubkey,
   };
   return kp;
+}
+
+export function calculateKeyPair<
+  S extends Scalar,
+  P extends Point<S>,
+  C extends Curve<S, P>
+>(c: C, signature: string): KeyPair<S, P> | undefined {
+  // Hasing the signature
+  const hash = utils.keccak256(signature);
+  let BN = BigNumber.from(hash);
+  let n = 0;
+  let found = false;
+  while (!found) {
+    try {
+      // Take the scalar from the number
+      BN = BN.add(n);
+      const priv = c.scalar().fromEvm(BN);
+      // Unwrap to make it owkr with the mul function above
+      const unwrapped = priv._unsafeUnwrap();
+      // Continue the calculation of the key
+      const pubkey = c.point().one().mul(unwrapped);
+      const kp: KeyPair<S, P> = {
+        secret: unwrapped,
+        pubkey: pubkey,
+      };
+      found = true;
+      return kp;
+    } catch (e) {
+      n++;
+    }
+  }
 }
 
 export function foo(a: number, b: number): number {
