e3ea089450fe4ce18e08858a57ed782f0c4be2f3
nikkolasg <nikkolasg@gmail.com>
2022-10-18 10:48:47 +0200

    using tweetnacl

--- src/encrypt.ts
+++ src/encrypt.ts
@@ -5,18 +5,20 @@ import { curve, G1 } from "../src/bn254";
 import { Ciphertext as HGamalCipher, EVMCipher as HGamalEVM } from "../src/hgamal";
 import * as hgamal from "./../src/hgamal";
 import { EVMEncoding } from "./encoding";
+import { secretbox, randomBytes } from "tweetnacl";
+import { encodeBase64, decodeBase64 } from "tweetnacl-util";
+
+const newNonce = () => randomBytes(secretbox.nonceLength);
+const generateKey = () => randomBytes(secretbox.keyLength);
 
 export class EncryptionBundle<KeyCipherEVM, KeyCipher extends EVMEncoding<KeyCipherEVM>> {
     /// data encrypted symmetrically, can be stored anywhere like IPFS
     encrypted_data: Uint8Array;
-    /// iv used to encrypt symmetrically 
-    iv: Uint8Array;
     /// key used to encrypt data, encrypted using Medusa, must be submitted to Medusa
     encrypted_key: KeyCipher;
-    constructor(d: Uint8Array, iv: Uint8Array, k: KeyCipher) {
+    constructor(d: Uint8Array, k: KeyCipher) {
         this.encrypted_data = d;
         this.encrypted_key = k;
-        this.iv = iv;
     }
 }
 
@@ -30,28 +32,17 @@ export class HGamalSuite<S extends Scalar, P extends Point<S>,C extends Curve<S,
     public async onetimeEncrypt(data: Uint8Array, medusa_key: P)
         : Promise<Result<EncryptionBundle<HGamalEVM, HGamalCipher<S,P>>,hgamal.EncryptionError>> {
         /// first encrypt the data symmetrically
-        let key = await window.crypto.subtle.generateKey(
-            {
-                name: "AES-GCM",
-                length: 256,
-            },
-            true,
-            ["encrypt", "decrypt"]);
-        let iv = window.crypto.getRandomValues(new Uint8Array(12));
-        let ciphertext = await window.crypto.subtle.encrypt(
-            {
-              name: "AES-GCM",
-              iv: iv
-            },
-            key,
-            data,
-          );
-        
+        const key = generateKey();
+        const nonce = newNonce();
+        const box = secretbox(data, nonce, key);
+        const fullMessage = new Uint8Array(nonce.length + box.length);
+        fullMessage.set(nonce);
+        fullMessage.set(box, nonce.length);
+
         /// then using the Medusa encryption
-        let keybuffer = await window.crypto.subtle.exportKey("raw",key);
-        let medusa_cipher = await hgamal.encrypt(this.curve, medusa_key, new Uint8Array(keybuffer));
+        let medusa_cipher = await hgamal.encrypt(this.curve, medusa_key, key);
         if (medusa_cipher.isOk()) {
-            return ok(new EncryptionBundle(new Uint8Array(ciphertext),iv,medusa_cipher.value));
+            return ok(new EncryptionBundle(fullMessage,medusa_cipher.value));
         } else {
             return err(medusa_cipher.error);
         }
@@ -80,17 +71,12 @@ export class HGamalSuite<S extends Scalar, P extends Point<S>,C extends Curve<S,
             if (!r.isOk()) {
                 return err(r.error);
             }
-            let key = await window.crypto.subtle.importKey( "raw", r.value, "AES-GCM",
-                                            true, ["encrypt", "decrypt"]);
+            let key = r.value;
             /// then decrypt the original data with this key
-            let data = await window.crypto.subtle.decrypt(
-                {
-                  name: "AES-GCM",
-                  iv: bundle.iv,
-                },
-                key,
-                bundle.encrypted_data,
-              );
-              return ok(new Uint8Array(data));
+            const nonce = bundle.encrypted_data.slice(0, secretbox.nonceLength);
+            const message = bundle.encrypted_data.slice(secretbox.nonceLength, bundle.encrypted_data.length);
+
+            const decrypted = secretbox.open(message, nonce, key);
+            return ok(decrypted);
         }
 }
