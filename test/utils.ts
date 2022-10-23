d095ad6710be9414ec4a41228661b9dded745a45
nikkolasg <nikkolasg@gmail.com>
2022-10-23 12:24:14 +0200

    updating contracts

--- test/utils.ts
+++ test/utils.ts
@@ -3,18 +3,18 @@ import { Curve, Point, Scalar } from "../src/algebra";
 import * as hgamal from "../src/hgamal";
 
 export function reencrypt<S extends Scalar, P extends Point<S>>(
-  c: Curve<S, P>,
-  kp: KeyPair<S, P>,
-  recipient: P,
-  cipher: hgamal.Ciphertext<S, P>
+    c: Curve<S, P>,
+    kp: KeyPair<S, P>,
+    recipient: P,
+    cipher: hgamal.Ciphertext<S, P>
 ): hgamal.MedusaReencryption<S, P> {
-  // Input is { rG, H(rP) ^ m }
-  // where P=pG is public key of proxy (kp)
-  // B=bG is the recipient key
-  // Output is
-  // prG + pB = p(rG + B)
-  // see hgamal script for more details
-  const random = c.point().set(recipient).add(cipher.random).mul(kp.secret);
-  const reenc = new hgamal.MedusaReencryption(random);
-  return reenc;
+    // Input is { rG, H(rP) ^ m }
+    // where P=pG is public key of proxy (kp)
+    // B=bG is the recipient key
+    // Output is
+    // prG + pB = p(rG + B)
+    // see hgamal script for more details
+    const random = c.point().set(recipient).add(cipher.random).mul(kp.secret);
+    const reenc = new hgamal.MedusaReencryption(random);
+    return reenc;
 }
