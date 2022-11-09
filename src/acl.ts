729bc3fae590da6bdc2a96c1a69b0036707de7e0
Jonathan Easterman <jonathaneasterman@gmail.com>
2022-11-09 13:15:27 +0000

    Remove unused acl.ts file

--- src/acl.ts
+++
@@ -1,7 +0,0 @@
-/* class ACL { */
-/* [>contract: Contract;<] */
-/* [>public static async instanceFrom(w: Web3, address: string) {<] */
-/* [>const acl_contract = new Contract(abi, address);<] */
-/* [>const oracleAddress = await contract.methods.getOracle().legacy().call();<] */
-/* [>}<] */
-/* } */
