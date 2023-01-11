24b5bb1ca1768e30800f5c1261257857774901e4
Nicolas Gailly <nikkolasg@users.noreply.github.com>
2023-01-11 15:36:06 +0100

    Automatically fetch public key during init
    Instead of a separate call to `fetchPublicKey` after init,
    we directly fetch it in the init method. This avoids one more
    call for the enduser.

--- src/index.ts
+++ src/index.ts
@@ -89,7 +89,9 @@ export class Medusa<S extends SecretKey, P extends PublicKey<S>> {
 
     switch (suite as SuiteType) {
       case SuiteType.BN254_KEYG1_HGAMAL:
-        return new Medusa(await initBn254(), signer, medusaAddress);
+        const medu = new Medusa(await initBn254(), signer, medusaAddress);
+        await medu.fetchPublicKey();
+        return medu;
       default:
         throw new Error(`unknown suite type: ${suite}`);
     }
